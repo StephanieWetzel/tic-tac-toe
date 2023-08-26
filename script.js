179ad1d89f2aa3f3947a65a9a62696e506cdb8e2
Stephanie Wetzel <st.wetzel96@hotmail.com>
2023-08-26 18:10:21 +0200

    determine winner

--- script.js
+++ script.js
@@ -10,6 +10,13 @@ let fields = [
     null // 8
 ];
 
+const WINNING_COMBINATIONS = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical
+    [0, 4, 8], [2, 4, 6], // diagonal
+];
+
+
 let currentPlayer = 'x';
 
 
@@ -49,7 +56,54 @@ function fillCell(cell, index) {
         cell.innerHTML = currentPlayer === 'o' ? generateAnimatedCircle() : generateAnimatedCross(); // Zelle wird gefüllt: wenn currentPlayer 'o' dann ...Circle() Funktion, wenn 'x' dann ...Cross() Funktion
         cell.onclick = null; // onclick-Funktion wird von der aktuellen Zelle wieder entfernt, damit bereits geklicktes Feld nicht nochmal geklickt werden kann
         currentPlayer = currentPlayer === 'x' ? 'o' : 'x'; // Spielerwechsel -> verkürzte Schreibweise für if/else: "if currentPlayer === x dann (?) 'o', ansonsten (:) 'x'"
+
+        if (isGameFinished()) {
+            const winCombination = getWinningCombination();
+            drawWinningLine(winCombination);
+        }
     }
+}
+
+
+function isGameFinished() {
+    return fields.every((field) => field !== null) || getWinningCombination() !== null;
+}
+
+function getWinningCombination() {
+    for (let i = 0; i < WINNING_COMBINATIONS.length; i++) {
+        const [a, b, c] = WINNING_COMBINATIONS[i];
+        if (fields[a] === fields[b] && fields[b] === fields[c] && fields[a] !== null) {
+            return WINNING_COMBINATIONS[i];
+        }
+    }
+    return null;
+}
+
+
+
+function drawWinningLine(combination) {
+    const lineColor = '#ffffff';
+    const lineWidth = 5;
+
+    const startCell = document.querySelectorAll(`td`)[combination[0]];
+    const endCell = document.querySelectorAll(`td`)[combination[2]];
+    const startRect = startCell.getBoundingClientRect();
+    const endRect = endCell.getBoundingClientRect();
+
+    const lineLength = Math.sqrt(
+        Math.pow(endRect.left - startRect.left, 2) + Math.pow(endRect.top - startRect.top, 2)
+    );
+    const lineAngle = Math.atan2(endRect.top - startRect.top, endRect.left - startRect.left);
+
+    const line = document.createElement('div');
+    line.style.position = 'absolute';
+    line.style.width = `${lineLength}px`;
+    line.style.height = `${lineWidth}px`;
+    line.style.backgroundColor = lineColor;
+    line.style.top = `${startRect.top + startRect.height / 2 - lineWidth / 2} px`;
+    line.style.left = `${startRect.left + startRect.width / 2} px`;
+    line.style.transform = `rotate(${lineAngle}rad)`;
+    document.getElementById('content').appendChild(line);
 }
 
 
@@ -79,4 +133,32 @@ function generateAnimatedCross() {
         </svg>
     `;
     return svgCode;
+}
+
+
+function checkWin() {
+    for (const pattern of winPatterns) {
+        const [a, b, c] = pattern;
+        if (fields[a] !== null && fields[a] === fields[b] && fields[a] === fields[c]) {
+            return pattern;
+        }
+    }
+    return null;
+}
+
+
+function drawWinLine(winPattern) {
+    const lineSvg = `
+        <svg width="220" height="220" xmlns="http://www.w3.org/2000/svg" style="position: absolute; left: -75px; top: -75px;">
+            <line x1="0" y1="0" x2="220" y2="220" stroke="white" stroke-width="4" />
+        </svg>
+    `;
+
+    const contentDiv = document.getElementById('content');
+    contentDiv.insertAdjacentHTML('beforeend', lineSvg);
+
+    for (const i of winPattern) {
+        const cell = document.getElementById(`cell-${i}`);
+        cell.style.backgroundColor = 'rgba(255, 255, 255, 0.3)';
+    }
 }
